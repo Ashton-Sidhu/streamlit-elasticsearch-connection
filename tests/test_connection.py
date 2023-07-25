86642fdad8f6ee42473278b824757185105ed35b
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2023-07-25 00:25:22 -0400

    Remove err test file

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -4,6 +4,9 @@ from elasticsearch_connection.connection import ElasticsearchConnection
 
 
 def test_init():
+    """Test if we can connect to ES cluster."""
 
     conn = ElasticsearchConnection("elasticsearch")
+
+    # Throws an error if connection fails.
     conn._raw_instance.info()
